bade4d91cb8f23c4425803fa5190ae2d078ea32f
“Alex” <trieu.alex1@gmail.com>
2019-06-30 18:18:10 -0700

    added color and price field to products

--- db/migrate/20190627065208_create_products.rb
+++ db/migrate/20190627065208_create_products.rb
@@ -4,7 +4,8 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.string :name
       t.text :description
       t.string :image_url
-
+      t.string :colour
+      t.decimal :price
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_065208) do
+ActiveRecord::Schema.define(version: 2019_07_01_003934) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_06_27_065208) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "colour"
+    t.decimal "price"
   end
 
 end
